29326eb3b83ca1a675b66acab6b920bfacd15dae
unknown <disha.karnataki@gmail.com>
2023-09-08 16:54:54 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors:
+      {
+        clockframe: '#6b65db'
+      }
+    },
+  },
+  plugins: [],
+}
